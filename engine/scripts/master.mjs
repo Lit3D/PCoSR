53bcf4f06d1431f7a3e6e4f07af562bc5b425bed
Wisdman <wisdman@ajaw.it>
2020-10-15 13:06:12 +0500

    update engine

--- engine/scripts/master.mjs
+++ engine/scripts/master.mjs
@@ -4,7 +4,7 @@ import { Scenario } from "./scenario.mjs"
 export class Master {
   #scenario = undefined
 
-  #newScenario = ({ detail: { id = "main", step = 0 } = {} }) => {
+  #newScenario = async ({ detail: { id = "main", step = 0 } = {} }) => {
     this.#scenario && this.#scenario.destruct()
     this.#scenario = await new Scenario(id)
     this.#scenario.step(step)
