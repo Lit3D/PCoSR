fb5c36c11c0116bd5b3189ada2e96c7575e4409d
Wisdman <wisdman@ajaw.it>
2020-11-25 01:09:53 +0500

    update

--- engine/scripts/main.mjs
+++ engine/scripts/main.mjs
@@ -1,13 +1,15 @@
 import { UUIDv4 } from "./uuid.mjs"
 
-const ID = (new URLSearchParams(window.location.search).get("id") || "").toLowerCase() || UUIDv4()
+const ID_AND_N = (new URLSearchParams(window.location.search).get("id") || "").toLowerCase() || UUIDv4()
+const {ID, N} = /^\s*(?<ID>\w+)(?:-?(?<N>\d+))?\s*$/.exec(ID_AND_N).groups
+
 const APP_MODULE = `./components/ss-app-${ID}/index.mjs`
 document.title += ` - ${ID}`
 document.body.classList.add(`sw--${window.screen.width}`)
 
 void async function main() {
   const { default: SSAppComponent } = await import(APP_MODULE)
-  document.body.appendChild(new SSAppComponent())
+  document.body.appendChild(new SSAppComponent(N))
 }().catch(err => {
   console.error(err)
   document.body.innerHTML = `<div class="error">MAIN ERROR: ${err}</div>`
