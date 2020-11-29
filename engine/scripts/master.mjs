7014bfbc860e6cb0fdd84ab60115f34dbf9fed7b
Wisdman <wisdman@ajaw.it>
2020-11-29 22:01:33 +0500

    add scenario loop

--- engine/scripts/master.mjs
+++ engine/scripts/master.mjs
@@ -34,6 +34,8 @@ export class Master {
       this.#qClient.publish(`${Q_PATH_LED}/video`, { src: led, muted: false }),
       this.#qClient.publish(`${Q_PATH_LINE}/wave`, { src: line }),
     ]).catch(err => console.error(`Master [VISUAL] error: ${err.message}`))
+
+
   }
 
   #currentScenario = undefined
