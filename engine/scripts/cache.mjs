e35f4fd6492c20b6ec0f3429a361b3de0a80b9e5
Wisdman <wisdman@ajaw.it>
2020-11-30 20:37:32 +0500

    add cache

---
+++ engine/scripts/cache.mjs
@@ -0,0 +1,21 @@
+export class Cache {
+  static _instance = new Cache()
+  constructor() { return Cache._instance ?? this }
+
+  get = url => {
+    if (this.#map.has(url)) return this.#map.get(url)
+    this.#fetch(url)
+        .then(() => console.debug(`Cache [fetch] success: ${url}`))
+        .catch(err => console.error(`Cache [fetch] error: ${err}`))
+    return url
+  }
+
+  #fetch = async url => {
+    const response = await fetch(url, { method: "GET", cache: "no-cache" })
+    const blob = await response.blob()
+    const data = URL.createObjectURL(blob)
+    this.#map.set(url, data)
+  }
+
+  clear = () => this.#map.clear()
+}
--- engine/scripts/realsense/env.mjs
+++ engine/scripts/realsense/env.mjs
@@ -1,4 +1,4 @@
 export const CONFIG_PATH = "/config/realsense.json"
 export const RECONNECT_TIMEOUT = 2 * 1000 // 2s
-export const TRAINING_DURATION = 30 * 1000 // 30sec
+export const TRAINING_DURATION = 60 * 1000 // 60sec
 export const DEFAULT_FRAME_SIZE = 100
